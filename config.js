f9db43fd691491478aba7cf8f0cecb19bba93ced
dlimpressions <restsaborvallenato@gmail.com>
2026-08-16 16:59:21 -0500

    Update API_BASE_URL to new endpoint

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://script.google.com/macros/s/TU_ID_DE_SCRIPT/exec';
+const API_BASE_URL = 'https://script.google.com/macros/s/AKfycbzrm0SOOJdR3naLj26x42SIyzOB5iecMWX5I0r4G4A0Sil6BdfJOpx3WPrXI9ShHKwH7Q/exec';
 function getConductorId() { return sessionStorage.getItem('conductorId'); }
 function setConductorId(id) { sessionStorage.setItem('conductorId', id); }
 function clearSession() { sessionStorage.removeItem('conductorId'); sessionStorage.removeItem('conductorNombre'); }
